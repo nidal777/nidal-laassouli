036f689da2791c474cf42d8907c8e0c008b4bf62
nidal777 <138388037+nidal777@users.noreply.github.com>
2023-07-03 09:12:06 +0100

    Add files via upload

--- Blink.ino
+++ Blink.ino
@@ -1,6 +1,8 @@
 /*
   Blink - Written by Mr.NIDAL LAASSOULI 
 
+  for more & more programms visit:  https://github.com/nidal777/nidal-laassouli
+
     - Turns an LED on for one second, then off for one second, repeatedly - 
 
 */
@@ -10,15 +12,18 @@ int led=2;
 
 // the setup function runs once when you press reset or power the board
 
-void setup() {
+void setup()
+             {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-}
+
+  pinMode(led, OUTPUT);
+             }
 
 // the loop function runs over and over again forever
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+void loop()
+            {
+  digitalWrite(led, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  digitalWrite(led, LOW);   // turn the LED off by making the voltage LOW
   delay(1000);                      // wait for a second
-}
+            }
